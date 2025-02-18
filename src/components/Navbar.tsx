b8dd27a15ba77278a200b3451b16d0e0e9b573bb
Wasim Khan <raju@ekfrazo.in>
2025-02-18 19:21:28 +0530

    Added page routes

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router";
+
+export default function Navbar() {
+    return (
+        <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    )
+}
